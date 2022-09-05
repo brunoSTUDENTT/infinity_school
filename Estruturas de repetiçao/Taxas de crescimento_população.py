fbe3094048d70638bdccf5e8ecccd7ba3bdaff52
Bruno Lopes <101883081+brunoSTUDENTT@users.noreply.github.com>
2022-09-05 11:36:02 -0300

    Update Taxas de crescimento_população.py

--- Estruturas de repetiçao/Taxas de crescimento_população.py
+++ Estruturas de repetiçao/Taxas de crescimento_população.py
@@ -18,3 +18,30 @@ while True:
     if pop_A >= pop_B:
         break
 print(i)
+
+#Continuação
+
+'''Altere o programa anterior permitindo ao usuário informar as populações e as taxas de crescimento iniciais.
+ Valide a entrada e permita repetir a operação.'''
+
+i = 0
+anos = 0
+while True:
+    pop_A = float(input('informe qtos habitantes tem A:'))
+    txA = float(input('Informe a taxa de crescimento da A:'))
+    pop_B = float(input('informe qtos habitantes tem B:'))
+    txB = float(input('Informe a taxa de crescimento da B:'))
+    while True:
+        pop_A = pop_A + (pop_A * txA )
+        pop_B = pop_B + (pop_B * txB)
+        i += 1
+        print(pop_A)
+        print(pop_B)
+        if pop_A >= pop_B:
+            break
+    print(i)
+    opcao = input('quer colocar mais um numero de habitantes e mais uma tx de crescimento? S/N').lower()
+    if opcao == 's':
+        continue
+    else:
+        break
